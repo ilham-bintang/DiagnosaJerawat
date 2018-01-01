d46c63b9bb42a5c606feb66d531a070c4aea469d
nullphantom <ilhambintanginformatika@gmail.com>
2018-01-01 21:06:09 +0800

    sudah fix tahap 2

--- app/src/main/java/io/github/nullphantom/diagnosajerawat/activity/About.java
+++ app/src/main/java/io/github/nullphantom/diagnosajerawat/activity/About.java
@@ -5,6 +5,7 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +13,7 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 import android.support.design.widget.Snackbar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -34,11 +36,11 @@ public class About extends AppCompatActivity {
 
         contactList = new ArrayList<>();
 
-        contactList.add(tambah("Aldian Wahyu Septiadi", "F1D015005", "aldianwahyu78@gmail.com", "bintang"));
-        contactList.add(tambah("Ilham Bintang", "F1D015035", "ilhambintanginformatika@gmail.com", "aldian"));
+        contactList.add(tambah("Aldian Wahyu Septiadi", "F1D015005", "aldianwahyu78@gmail.com", "aldian"));
+        contactList.add(tambah("Ilham Bintang", "F1D015035", "ilhambintanginformatika@gmail.com", "bintang"));
         contactList.add(tambah("Nurhalimah", "F1D015063", "nurh5h@gmail.com", "halimah"));
-        contactList.add(tambah("Ririn Perwitasari", "F1D015075", "ririnperwita@gmail.com", "g01"));
-        contactList.add(tambah("Rosida Apriani", "F1D015079", "ocioci@gmail.com", "g01"));
+        contactList.add(tambah("Ririn Perwitasari", "F1D015075", "ririnperwita@gmail.com", "ririn"));
+        contactList.add(tambah("Rosida Apriani", "F1D015079", "ocioci@gmail.com", "oci"));
 
         parentView = findViewById(R.id.parentLayout);
 
@@ -69,5 +71,26 @@ public class About extends AppCompatActivity {
         x.setEmail(email);
         x.setGambar(gambar);
         return x;
+    }
+
+    public void kirim_email(View v) {
+       String[] TO = {"ilhambintanginformatika@gmail.com"};
+       String[] CC = {"aldianwahyuseptiadi@gmail.com"};
+       String subject = "Kontak Untuk Aplikasi Sistem Pakar Diagnosa Penyakit Jerawat";
+       Intent i = new Intent(Intent.ACTION_SEND);
+
+       i.setData(Uri.parse("mailto : "));
+       i.setType("text/plain");
+       i.putExtra(Intent.EXTRA_EMAIL, TO);
+       i.putExtra(Intent.EXTRA_CC, CC);
+       i.putExtra(Intent.EXTRA_SUBJECT, subject);
+
+       try {
+           startActivity(Intent.createChooser(i, "kirim email.."));
+           finish();
+       } catch (android.content.ActivityNotFoundException e) {
+           Toast.makeText(About.this, "Tidak ada aplikasi email yang ada", Toast.LENGTH_SHORT).show();
+       }
+
     }
 }
